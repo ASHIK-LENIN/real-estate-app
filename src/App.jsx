39f3d4783977010e866471a612b8f75535deb9e8
Ashik Lenin <ashikleninn@gamil.com>
2023-12-12 21:12:10 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import SignIn from "./Pages/Signin"
+import SignUp from "./Pages/SignUp"
+import About from "./Pages/About"
+import Profile from "./Pages/Profile"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path="/" element={ <Home />  } />
+      <Route path="/signin" element={ <SignIn />  } />
+      <Route path="/signup" element={ <SignUp />  } />
+      <Route path="/about" element={ <About />  } />
+      <Route path="/profile" element={ <Profile />  } />
+
+
+    </Routes>
+
+
+    </>
   )
 }
 
