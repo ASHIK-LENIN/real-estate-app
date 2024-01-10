9045d8ca9d763db07a4f77503717d09ab9e5a192
Ashik Lenin <ashikleninn@gamil.com>
2024-01-10 11:15:29 +0530

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./Components/PrivateRoute"
 import CreateListing from "./Pages/CreateListing"
 import UpdateListing from "./Pages/UpdateListing"
 import Listing from "./Pages/Listing"
+import Search from "./Pages/Search"
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
       <Route path="/login" element={ <SignIn />  } />
       <Route path="/signup" element={ <SignUp />  } />
       <Route path="/about" element={ <About />  } />
+      <Route path="/search" element={ <Search />  } />
       <Route path="/listing/:listingId" element={ <Listing />  } />
 
       <Route  element={ <PrivateRoute />  } >
